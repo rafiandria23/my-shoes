d3a19b8f7e33b88c7c059f7ae4e22e374d4cfd3a
Adam Rafiandri <rafiandria23@gmail.com>
2020-01-30 23:26:54 +0700

    Added express static

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const port = 3000;
 const user = require("./routes/user-route")
 
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
